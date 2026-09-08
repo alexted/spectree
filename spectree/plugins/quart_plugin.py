98ed4cbc0ea92b9eb2e78f9a29a59086d25e7352
Alex Ted <alextedx@gmail.com>
2026-09-08 23:18:40 +0300

    refactor: normalize Quart request data

--- spectree/plugins/quart_plugin.py
+++ spectree/plugins/quart_plugin.py
@@ -27,10 +27,20 @@ class QuartPlugin(WerkzeugPlugin):
     def is_blueprint(app: Any) -> bool:
         return isinstance(app, Blueprint)
 
-    async def get_request_data(self, request, endpoint: EndpointSpec) -> RequestData:
+    async def get_request_data(
+        self, request, endpoint: EndpointSpec
+    ) -> RequestData:
         has_data = request.method not in ("GET", "DELETE")
-        use_json = endpoint.json and has_data and request.mimetype == "application/json"
-        use_form = endpoint.form and has_data and any(x in request.mimetype for x in self.FORM_MIMETYPE)
+        use_json = (
+            endpoint.json
+            and has_data
+            and request.mimetype == "application/json"
+        )
+        use_form = (
+            endpoint.form
+            and has_data
+            and any(x in request.mimetype for x in self.FORM_MIMETYPE)
+        )
 
         req_form = None
         if use_form:
@@ -40,15 +50,27 @@ class QuartPlugin(WerkzeugPlugin):
             if files:
                 req_form.update(get_multidict_items(files, endpoint.form))
 
+        req_json = None
+        if use_json:
+            req_json = await request.get_json(silent=True)
+            if req_json is None:
+                req_json = {}
+
         return RequestData(
             query=get_multidict_items(request.args, endpoint.query),
-            json=(await request.get_json(silent=True) or {}) if use_json else None,
+            json=req_json,
             form=req_form,
-            headers=dict(request.headers) or {},
-            cookies=get_multidict_items(request.cookies) or {},
+            headers=dict(request.headers),
+            cookies=get_multidict_items(request.cookies),
         )
 
-    async def validate_response(self, resp, resp_model: Response | None, skip_validation: bool, force_resp_serialize: bool):
+    async def validate_response(
+        self,
+        resp,
+        resp_model: Response | None,
+        skip_validation: bool,
+        force_resp_serialize: bool,
+    ):
         resp_validation_error = None
         payload, status, additional_headers = flask_response_unpack(resp)
         if self.is_app_response(payload):
@@ -60,24 +82,40 @@ class QuartPlugin(WerkzeugPlugin):
             additional_headers = resp_headers
         if not skip_validation and resp_model:
             try:
-                result = validate_response(self.model_adapter, resp_model.find_model(status), payload, force_resp_serialize)
+                result = validate_response(
+                    self.model_adapter,
+                    resp_model.find_model(status),
+                    payload,
+                    force_resp_serialize,
+                )
             except self.model_adapter.validation_error as err:
-                response = await make_response(self.model_adapter.validation_errors(err), 500)
+                response = await make_response(
+                    self.model_adapter.validation_errors(err), 500
+                )
                 resp_validation_error = err
             else:
                 response = await make_response(
-                    self.get_current_app().response_class(result.payload, mimetype="application/json")
-                    if isinstance(result.payload, bytes) else result.payload,
+                    self.get_current_app().response_class(
+                        result.payload,
+                        mimetype="application/json",
+                    )
+                    if isinstance(result.payload, bytes)
+                    else result.payload,
                     status,
                     additional_headers,
                 )
         else:
             if self.model_adapter.is_partial_model_instance(payload):
-                payload = self.get_current_app().response_class(self.model_adapter.dump_json(payload), mimetype="application/json")
+                payload = self.get_current_app().response_class(
+                    self.model_adapter.dump_json(payload),
+                    mimetype="application/json",
+                )
             response = await make_response(payload, status, additional_headers)
         return response, resp_validation_error
 
-    async def validate(self, func: Callable, endpoint: EndpointSpec, *args: Any, **kwargs: Any):
+    async def validate(
+        self, func: Callable, endpoint: EndpointSpec, *args: Any, **kwargs: Any
+    ):
         request_data = await self.get_request_data(request, endpoint)
         response = None
         req_validation_error = None
@@ -87,13 +125,39 @@ class QuartPlugin(WerkzeugPlugin):
             self.set_request_data(request, request_data)
         except self.model_adapter.validation_error as err:
             req_validation_error = err
-            response = await make_response(jsonify(self.model_adapter.validation_errors(err)), endpoint.validation_error_status)
-        endpoint.before(request, response, req_validation_error, None, self.model_adapter)
+            response = await make_response(
+                jsonify(self.model_adapter.validation_errors(err)),
+                endpoint.validation_error_status,
+            )
+
+        endpoint.before(
+            request,
+            response,
+            req_validation_error,
+            None,
+            self.model_adapter,
+        )
         if req_validation_error is not None:
             assert response
             abort(response)
+
         self.inject_request_data(request_data, endpoint, kwargs)
-        result = await func(*args, **kwargs) if inspect.iscoroutinefunction(func) else func(*args, **kwargs)
-        response, resp_validation_error = await self.validate_response(result, endpoint.response, endpoint.skip_validation, endpoint.force_resp_serialize)
-        endpoint.after(request, response, resp_validation_error, None, self.model_adapter)
+        result = (
+            await func(*args, **kwargs)
+            if inspect.iscoroutinefunction(func)
+            else func(*args, **kwargs)
+        )
+        response, resp_validation_error = await self.validate_response(
+            result,
+            endpoint.response,
+            endpoint.skip_validation,
+            endpoint.force_resp_serialize,
+        )
+        endpoint.after(
+            request,
+            response,
+            resp_validation_error,
+            None,
+            self.model_adapter,
+        )
         return response
