c5e90e4afa91af18a70d7c6e30b44c5131f627d5
Alex Ted <alextedx@gmail.com>
2026-09-08 17:51:23 +0300

    refactor: remove redundant Flask base import

--- spectree/plugins/flask_plugin.py
+++ spectree/plugins/flask_plugin.py
@@ -5,7 +5,7 @@ import flask
 from flask import Blueprint, abort, current_app, jsonify, make_response, request
 
 from spectree.endpoint import EndpointSpec
-from spectree.plugins.base import BasePlugin, validate_response
+from spectree.plugins.base import validate_response
 from spectree.request_data import RequestData
 from spectree.plugins.werkzeug_utils import WerkzeugPlugin, flask_response_unpack
 from spectree.response import Response
@@ -27,7 +27,6 @@ class FlaskPlugin(WerkzeugPlugin):
         has_data = request.method not in ("GET", "DELETE")
         use_json = endpoint.json and has_data and request.mimetype not in self.FORM_MIMETYPE
         use_form = endpoint.form and has_data and request.mimetype in self.FORM_MIMETYPE
-
         return RequestData(
             query=get_multidict_items(request.args, endpoint.query),
             json=(request.get_json(silent=True) or {}) if use_json else None,
@@ -36,16 +35,9 @@ class FlaskPlugin(WerkzeugPlugin):
             cookies=get_multidict_items(request.cookies),
         )
 
-    def validate_response(
-        self,
-        resp,
-        resp_model: Response | None,
-        skip_validation: bool,
-        force_resp_serialize: bool,
-    ):
+    def validate_response(self, resp, resp_model: Response | None, skip_validation: bool, force_resp_serialize: bool):
         resp_validation_error = None
         payload, status, additional_headers = flask_response_unpack(resp)
-
         if self.is_app_response(payload):
             resp_status, resp_headers = payload.status_code, payload.headers
             payload = payload.get_data()
@@ -53,87 +45,44 @@ class FlaskPlugin(WerkzeugPlugin):
                 status = resp_status
             resp_headers.extend(additional_headers)
             additional_headers = resp_headers
-
         if not skip_validation and resp_model:
             try:
-                response_validation_result = validate_response(
-                    model_adapter=self.model_adapter,
-                    validation_model=resp_model.find_model(status),
-                    response_payload=payload,
-                    force_serialize=force_resp_serialize,
-                )
+                result = validate_response(self.model_adapter, resp_model.find_model(status), payload, force_resp_serialize)
             except self.model_adapter.validation_error as err:
-                errors = self.model_adapter.validation_errors(err)
-                response = make_response(errors, 500)
+                response = make_response(self.model_adapter.validation_errors(err), 500)
                 resp_validation_error = err
             else:
                 response = make_response(
-                    self.get_current_app().response_class(
-                        response_validation_result.payload,
-                        mimetype="application/json",
-                    )
-                    if isinstance(response_validation_result.payload, bytes)
-                    else response_validation_result.payload,
+                    self.get_current_app().response_class(result.payload, mimetype="application/json")
+                    if isinstance(result.payload, bytes) else result.payload,
                     status,
                     additional_headers,
                 )
         else:
             if self.model_adapter.is_partial_model_instance(payload):
-                payload = self.get_current_app().response_class(
-                    self.model_adapter.dump_json(payload),
-                    mimetype="application/json",
-                )
+                payload = self.get_current_app().response_class(self.model_adapter.dump_json(payload), mimetype="application/json")
             response = make_response(payload, status, additional_headers)
-
         return response, resp_validation_error
 
     def validate(self, func: Callable, endpoint: EndpointSpec, *args: Any, **kwargs: Any):
         if request.url_rule is not None and getattr(request.url_rule, "websocket", False):
             return func(*args, **kwargs)
-
-        request_data = RequestData()
+        request_data = self.get_request_data(request, endpoint)
         response = None
         req_validation_error = None
-
         try:
-            request_data = self.get_request_data(request, endpoint)
             if not endpoint.skip_validation:
                 request_data = self.validate_request_data(request_data, endpoint)
             self.set_request_data(request, request_data)
         except self.model_adapter.validation_error as err:
             req_validation_error = err
-            response = make_response(
-                jsonify(self.model_adapter.validation_errors(err)),
-                endpoint.validation_error_status,
-            )
-
-        endpoint.before(
-            request,
-            response,
-            req_validation_error,
-            None,
-            self.model_adapter,
-        )
-
+            response = make_response(self.model_adapter.validation_errors(err), endpoint.validation_error_status)
+        endpoint.before(request, response, req_validation_error, None, self.model_adapter)
         if req_validation_error is not None:
             assert response
             abort(response)
-
         self.inject_request_data(request_data, endpoint, kwargs)
         result = func(*args, **kwargs)
-
-        response, resp_validation_error = self.validate_response(
-            result,
-            endpoint.response,
-            endpoint.skip_validation,
-            endpoint.force_resp_serialize,
-        )
-
-        endpoint.after(
-            request,
-            response,
-            resp_validation_error,
-            None,
-            self.model_adapter,
-        )
+        response, resp_validation_error = self.validate_response(result, endpoint.response, endpoint.skip_validation, endpoint.force_resp_serialize)
+        endpoint.after(request, response, resp_validation_error, None, self.model_adapter)
         return response
